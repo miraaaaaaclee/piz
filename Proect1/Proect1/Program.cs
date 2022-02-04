83420eb1e5e75ad8ef649bd9022151f9c0629daf
Daria <98027855+miraaaaaaclee@users.noreply.github.com>
2022-02-04 12:32:49 +0300

    Add files via upload

---
+++ Proect1/Proect1/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+string name, surname, adress, country, course_name, teacher;
+int point, week_lessons;
+string birthday = "2004 05 21";
+string gender = "f";
+name = "Leyla";
+surname = "Ismoilova";
+adress = "Moscow city";
+country = "RUSSIA";
+course_name = "Разработка программных модулей";
+teacher = "Жукова И.А.";
+point = 100;
+week_lessons = 5;
+
+Console.WriteLine("Информация о студенте");
+Console.WriteLine("\nИмя: " + name + "\nФамилия: " + surname + "\nДата рождения: " + birthday + "\nАдрес: " + adress + "\nПол: " + gender + "\nСтрана: " + country);
+Console.WriteLine();
+Console.WriteLine("Информация о курсе");
+Console.WriteLine("\nНазвание курса: " + course_name + "\nБаллы: " + point + "\nКоличество занятий в неделе: " + week_lessons + "\nПреподаватель: " + teacher);
+Console.ReadKey();
+*/
+
+/*int n = int.Parse(Console.ReadLine());
+n++;
+Console.WriteLine(n);
+Console.ReadKey();
+*/
+
+/*int FirstNum = int.Parse(Console.ReadLine());
+double SecondNum = int.Parse(Console.ReadLine());
+double avg = (FirstNum + SecondNum) / 2;
+Console.WriteLine(avg);
+Console.ReadKey();*/
+
+/*Console.WriteLine("Введите сторону треугольника:\n ");
+double stor = int.Parse(Console.ReadLine());
+double sqrt = Math.Sqrt(3);
+
+Console.WriteLine($"Периметр: {stor * 3}");
+Console.WriteLine($"Площадь: {(stor * stor * sqrt) / 4}");
+Console.ReadKey();*/
+
+/*Console.WriteLine("Введите четыре числа: ");
+int x1 = int.Parse(Console.ReadLine());
+int x2 = int.Parse(Console.ReadLine());
+int x3 = int.Parse(Console.ReadLine());
+int x4 = int.Parse(Console.ReadLine());
+int sum1, sum2;
+sum1 = x1 + x2;
+sum2 = x2 + x3;
+Console.WriteLine(sum1 > sum2);
+Console.ReadKey();*/
