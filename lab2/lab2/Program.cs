02c2143468128b6f37c86c8aac91044ec4364579
Daria <98027855+miraaaaaaclee@users.noreply.github.com>
2022-02-11 09:56:45 +0300

    Add files via upload

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,92 @@
+﻿/*Console.WriteLine("Введите число");
+int numb = Int32.Parse(Console.ReadLine());
+if (numb > 0)
+{
+    Console.WriteLine("Введенное число положительное");
+}
+else
+{
+    Console.WriteLine("Введенное число отрицательное");
+}*/
+
+/*Console.WriteLine("Введите десятичное число");
+int number = Int32.Parse(Console.ReadLine());
+Console.WriteLine(number % 10);
+Console.WriteLine(number /= 10);
+Console.ReadKey();*/
+
+/*Console.WriteLine("Введите трехразрядное число");
+int number = Int32.Parse(Console.ReadLine());
+int s, d, b, o, f;
+b = number % 10;
+s = number /= 100;
+f = number - s * 100 - b;
+o = f * 0;
+Console.WriteLine(s * 100 + o* 10 + b);
+Console.ReadKey(); */
+
+/*Console.WriteLine("Введите три числа");
+int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+int c = Int32.Parse(Console.ReadLine());
+if ((a != b) && (b != c) && (a != c))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}*/
+/*Console.WriteLine("Введите три числа");
+int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+int c = Int32.Parse(Console.ReadLine());
+double p, s;
+p = (a + b + c) / 2;
+s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+if (a + b > c
+            || a + c > b
+            || b + c > a)
+{
+    Console.WriteLine($"треугольник существует, площадь треугольника:  {p} {s}");
+    
+}
+else
+{
+    Console.WriteLine("треугольник не существует");
+}*/
+
+/*Console.WriteLine("Введите x");
+int x = Int32.Parse(Console.ReadLine());
+if ((x < -2) | (x > 2))
+{ Console.WriteLine(x*2); }
+else
+{ Console.WriteLine(-3 * x); }*/
+
+/*Console.WriteLine("Введите x");
+int x = Int32.Parse(Console.ReadLine());
+if (x > 0)
+{ Console.WriteLine(2 * Math.Sin(x)); }
+else
+{ Console.WriteLine(6 - x); }*/
+/*
+Console.WriteLine("Введите номер дня недели");
+int n= Int32.Parse(Console.ReadLine());
+if (n == 1)
+{ Console.WriteLine("Понедельник"); }
+else if (n == 2)
+{ Console.WriteLine("Вторник"); }
+else if (n == 3)
+{ Console.WriteLine("Среда"); }
+else if (n == 4)
+{ Console.WriteLine("Четверг"); }
+else if (n == 5)
+{ Console.WriteLine("Пятница"); }
+else if (n == 6)
+{ Console.WriteLine("Суббота"); }
+else
+{ Console.WriteLine("Воскресенье"); }
+*/
+
+
+
