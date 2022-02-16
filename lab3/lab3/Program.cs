1396a8a28e4304d9004ae43a3741ed4d9b749332
Daria <98027855+miraaaaaaclee@users.noreply.github.com>
2022-02-16 12:45:57 +0300

    Add files via upload

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,73 @@
+﻿/*for (int counter = -3; counter <= 24; counter+=3)
+{
+    Console.WriteLine($"Счетчик: {counter}");
+}*/
+
+/*for (int counter = 1; counter <= 100; counter ++)
+{
+    Console.WriteLine($"{counter}");
+}
+for (int counter = 99; counter >= 1; counter--)
+{
+    Console.WriteLine($"{counter}");
+}*/
+
+/*int m = 0;
+int a = 0;
+int b = 0;
+for (int n=1; n<10; n++ )
+{
+    m = int.Parse(Console.ReadLine());
+    if (m > 0)
+    { a++; } 
+    else
+    { b++; }
+}
+Console.WriteLine($"число положительных {a}");
+Console.WriteLine($"число отрицательных {b}");*/
+
+/*int m = 0;
+int sum = 0;
+for (int n = 1; n < 10; n++)
+{
+    m = int.Parse(Console.ReadLine());
+    sum += m;
+}
+Console.WriteLine(sum);*/
+
+
+/*int sum = 0;
+for (int counter = 1; counter <= 19; counter += 2)
+{
+  
+    sum += counter;
+}
+Console.WriteLine(sum);*/
+
+/*double product = 1.0;
+double m;
+for (int n = 1; n < 10; n++)
+{
+    m = double.Parse(Console.ReadLine());
+    product *= m; }
+Console.WriteLine(product);*/
+
+/*int res=0;
+for(int x = 2; x <= 8; x++)
+{
+    for (int y = 2; y <= 5; y++)
+    {
+        res= (int)Math.Pow(x, y);
+        Console.WriteLine(res);
+    }
+}*/
+
+/*int res=0;
+for (int x = 30; x <= 33; x++)
+{
+    for (int y = 1; y <= 5; y++)
+    {
+        res = x - y;
+        Console.WriteLine(res);
+    }
+}*/
