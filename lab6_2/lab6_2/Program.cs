d1c11c4375a765cf04a6d2a70665f7da6aa030cf
Daria <98027855+miraaaaaaclee@users.noreply.github.com>
2022-03-04 10:35:20 +0300

    Add files via upload

---
+++ lab6_2/lab6_2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*zad_2*/
+
+/*Console.WriteLine("Введите натуральные числа a и N");
+int a = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+int res = a;
+for (int i = 1; i <= N; i++)
+{
+    res++;
+}
+IEnumerable<int> seq;
+seq = System.Linq.Enumerable.Range(a, N);
+foreach (var x in seq)
+    Console.WriteLine($"{x} ");*/
+
+/*zad_3*/
+/*int a = 1;
+int b = 9;
+int result = 0;
+for (int i = 1; i <= b; i++)
+{
+    result += 2;
+}
+IEnumerable<int> seq;
+seq = System.Linq.Enumerable.Range(a, result);
+foreach (var x in seq)
+    if (x % 2 != 0)
+    {
+        Console.WriteLine($"{x} ");
+    }*/
