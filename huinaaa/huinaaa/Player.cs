cceadf970cd625ae49fffd730d01d37a4a8f172a
Daria <98027855+miraaaaaaclee@users.noreply.github.com>
2022-04-29 13:37:19 +0300

    10.1

---
+++ huinaaa/huinaaa/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace huinaaa
+{
+     class Player
+    {
+        public string Name { get; set; }
+        public string Skill { get; set; }
+    }
+}
